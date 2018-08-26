1eb9738aa9d572cb49120ed3e1d32cc807494883
enzefalon <schuefer@gmail.com>
2018-08-26 23:26:40 +0200

    extended base scss: added grid system and helpers/ utilities

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container-fluid">
+      <div class="row">
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
